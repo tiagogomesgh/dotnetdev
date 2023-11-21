f8b020c5b041ff18e8bacfa90bfd29a761bae9b4
Tiago Gomes <91081189+tiagogomesgh@users.noreply.github.com>
2023-11-20 18:16:43 -0800

    Created ConsoleApp + MVC App through JetBrains Rider for testing.

---
+++ ConsoleApp/ConsoleAppProj/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please Enter Your Full Name.");
+string fullName = Console.ReadLine();
+string[]splitNames = fullName.Split(" ");
+Console.WriteLine($"User Information: First Name: {splitNames[0]} Last Name: {splitNames[1]}");
+
